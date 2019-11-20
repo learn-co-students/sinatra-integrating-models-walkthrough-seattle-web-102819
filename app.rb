377de19e0915b640d9343900e9e5d27bf12a995a
github username <github email address>
2019-11-19 17:49:17 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,22 @@
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
+
 class App < Sinatra::Base
+
   get '/' do
     erb :index
   end
 
-  post '/' do
+    post '/' do
     text_from_user = params[:user_text]
-
+    @analyzed_text = TextAnalyzer.new(text_from_user)
+      @count = @analyzed_text.count_of_words.to_s
+      a = @analyzed_text.most_common_letter
+      @letter = a[0]
+      @letter_count = a[1]
     erb :results
   end
+
+
 end
--- models/text_analyzer.rb
+++ models/text_analyzer.rb
@@ -1,1 +1,36 @@
 # Your TextAnalyzer model code will go here.
+class TextAnalyzer
+    attr_reader :text
+   
+    def initialize(text)
+      @text = text.downcase
+    end
+   
+    def count_of_words
+      words = text.split(" ")
+      words.count
+    end
+   
+    def count_of_vowels
+      text.scan(/[aeoui]/).count
+    end
+   
+    def count_of_consonants
+      text.scan(/[bcdfghjklmnpqrstvwxyz]/).count
+    end
+   
+    def most_common_letter
+        chars             = {}
+        most_common       = nil
+        most_common_count = 0
+        text.downcase.gsub(/[^a-z]/, '').each_char do |c|
+          count = (chars[c] = (chars[c] || 0) + 1)
+          if count > most_common_count
+            most_common       = c
+            most_common_count = count
+          end
+        end
+        [most_common, most_common_count]
+      end
+
+  end
